bddc002dbfb93eb87e7e0eea92a27d4553166054
haoxiang <hg2412@columbia.edu>
2018-03-22 22:21:42 -0400

    updated regularization and attention on input time and sensors

--- keras_model_factory.py
+++ keras_model_factory.py
@@ -1,6 +1,7 @@
+from keras import Model
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
-from layers import att_time_cls
+from keras.layers import Input, Dense, LSTM, RNN, Embedding, Concatenate, Multiply
+from layers import att_time_cls, att_input_rnn
 
 
 def create_lstm_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
@@ -13,8 +14,35 @@ def create_lstm_model(batch_size, num_hidden_units, num_steps, num_features, num
 
 def create_attention_time_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
     model = Sequential()
-    model.add(LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True, stateful=False))
+    model.add(
+        LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True,
+             stateful=False))
     model.add(att_time_cls.Attention([batch_size, num_hidden_units], batch_size=batch_size))
     model.add(Dense(num_classes, batch_size=batch_size, activation="softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def create_attention_time_continuous_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
+    model = Sequential()
+    model.add(
+        LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True,
+             stateful=False))
+    model.add(att_time_cls.Attention([batch_size, num_hidden_units], batch_size=batch_size, continuous=True))
+    model.add(Dense(num_classes, batch_size=batch_size, activation="softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+def create_attention_input_rnn_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
+    main_input = Input(batch_shape=(batch_size, num_steps, num_features), name='main_input')
+    h = LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True)(main_input)
+
+    att = att_input_rnn.Attention(n_feature=num_features, n_sensor=3, batch_size=batch_size)(h)
+    x = Multiply()([main_input, att])
+    lstm_out = LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size,
+                    return_sequences=False)(x)
+    out = Dense(num_classes, batch_size=batch_size, activation="softmax")(lstm_out)
+    model = Model(inputs=[main_input], outputs=[out])
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
