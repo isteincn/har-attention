62803ee37bf642512e49becf496e38059885e383
haoxiang <hg2412@columbia.edu>
2018-03-31 21:57:01 -0400

    refactored training script

--- keras_model_factory.py
+++ keras_model_factory.py
@@ -5,15 +5,14 @@ from layers import att_time_cls, att_input_rnn, att_input_multihead
 
 
 def create_lstm_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
-    model = Sequential()
+    model = Sequential(name="lstm")
     model.add(LSTM(num_hidden_units, input_shape=(num_steps, num_features), return_sequences=False, stateful=False))
     model.add(Dense(num_classes, activation="softmax"))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-
 def create_attention_time_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
-    model = Sequential()
+    model = Sequential(name="attention_hidden")
     model.add(
         LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True,
              stateful=False))
@@ -24,7 +23,7 @@ def create_attention_time_model(batch_size, num_hidden_units, num_steps, num_fea
 
 
 def create_attention_time_continuous_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
-    model = Sequential()
+    model = Sequential(name="attention_hidden_continuous")
     model.add(
         LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True,
              stateful=False))
@@ -43,7 +42,7 @@ def create_attention_input_rnn_model(batch_size, num_hidden_units, num_steps, nu
     lstm_out = LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size,
                     return_sequences=False)(x)
     out = Dense(num_classes, batch_size=batch_size, activation="softmax")(lstm_out)
-    model = Model(inputs=[main_input], outputs=[out])
+    model = Model(inputs=[main_input], outputs=[out], name="attention_input_rnn")
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
@@ -58,7 +57,7 @@ def create_attention_input_rnn_continuous_model(batch_size, num_hidden_units, nu
     lstm_out = LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size,
                     return_sequences=False)(x)
     out = Dense(num_classes, batch_size=batch_size, activation="softmax")(lstm_out)
-    model = Model(inputs=[main_input], outputs=[out])
+    model = Model(inputs=[main_input], outputs=[out], name="attention_input_rnn_continuous")
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
@@ -71,6 +70,23 @@ def create_attention_input_multihead_model(batch_size, num_hidden_units, num_ste
     lstm_out = LSTM(num_hidden_units, input_shape=(num_steps, num_heads * 3), batch_size=batch_size,
                     return_sequences=False)(att_out)
     out = Dense(num_classes, batch_size=batch_size, activation="softmax")(lstm_out)
-    model = Model(inputs=[main_input], outputs=[out])
+    model = Model(inputs=[main_input], outputs=[out], name="attention_multihead")
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+# att only multiply without weighted sum, and added L1 L2 regularization
+def create_attention_input_rnn_hidden_continuous_model(batch_size, num_hidden_units, num_steps, num_features, num_classes):
+    main_input = Input(batch_shape=(batch_size, num_steps, num_features), name='main_input')
+    h = LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size, return_sequences=True)(main_input)
+
+    att = att_input_rnn.Attention(n_feature=num_features, n_sensor=3, continuous=True, batch_size=batch_size)(h)
+    x = Multiply()([main_input, att])
+    lstm_out = LSTM(num_hidden_units, input_shape=(num_steps, num_features), batch_size=batch_size,
+                    return_sequences=True)(x)
+
+    att_hidden = att_time_cls.Attention([batch_size, num_hidden_units], batch_size=batch_size, continuous=True)(lstm_out)
+    out = Dense(num_classes, batch_size=batch_size, activation="softmax")(att_hidden)
+    model = Model(inputs=[main_input], outputs=[out], name="attention_input_rnn_hidden_continuous")
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
--- keras_pamap2_train.py
+++ keras_pamap2_train.py
@@ -5,53 +5,74 @@ Created on Sun Feb 25 14:29:34 2018
 LSTM on pamap2 dataset
 @author: haoxiang
 """
-
+import os
 import data.pamap2_data as pamap2_data
 import numpy as np
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM
 from keras_model_factory import *
 from sklearn.metrics import f1_score
 
 # hyper params
-num_steps = 256
-num_layers = 2
+num_steps = 64
 num_features = len(pamap2_data.FEATURE_NAMES)
 num_classes = pamap2_data.NUM_CLASSES
 num_hidden_units = 64
-num_epochs = 1000
 batch_size = 512
-epoch_size = 5
 num_heads = 12
 
 
-def train(model, data):
+def train(model, data, random_batch=False, num_epochs=20, load=True):
+    # calc num of batches per epoch
+    epoch_size = data.df_train.shape[0] / (num_steps / 2) / batch_size
+
+    # load model if model file exists
+    if load and os.path.exists(model.name + ".h5"):
+        model = load_model(model.name + '.h5')
+
     for epoch in range(num_epochs):
         tr_losses = []
         tr_accs = []
         for _ in xrange(epoch_size):
-            x, y = data.get_random_batch("train", batch_size, num_steps, one_hot=True)
+            if random_batch:
+                x, y = data.get_random_batch("train", batch_size, num_steps, one_hot=True)
+            else:
+                x, y = data.get_next_batch("train", batch_size, num_steps, one_hot=True)
             x = np.array(x)
             tr_loss, tr_acc = model.train_on_batch(x, y)
             tr_losses.append(tr_loss)
             tr_accs.append(tr_acc)
-
         print("=====" * 20)
-        print("\t".join(["Epoch Train: ", str(epoch), " Loss:", str(sum(tr_losses) / epoch_size), " Acc: ",  str(sum(tr_accs) / epoch_size)]))
-        # print acc on val data set
-        x, y = data.get_random_batch("validate", batch_size, num_steps, one_hot=True)
-        x_arr = np.array(x)
-        ts_loss, ts_acc = model.test_on_batch(x_arr, y)
-        print("\t".join(["Epoch Val: ", str(epoch), " Loss:", str(ts_loss), " Acc: ", str(ts_acc)]))
-        y_pred = np.argmax(model.predict_on_batch(x_arr), axis=1)
-        y_true = np.argmax(y, axis=1)
-        print("\t".join(["F1 Score: ", str(f1_score(y_true, y_pred, average="macro"))]))
-
+        print("Epoch " + str(epoch))
+        print("Train Acc: %f\tLoss: %f\t" % (sum(tr_accs) / epoch_size, sum(tr_losses) / epoch_size))
+        test(model, data)
         print("=====" * 20)
+        model.save(model.name + '.h5')
+    return
 
 
-# model_lstm = create_lstm_model(batch_size, num_hidden_units, num_steps, num_features, num_classes)
-# train(model_lstm, pamap2_data)
+def test(model, data):
+    epoch_size = data.df_train.shape[0] / (num_steps / 2) / batch_size # num of batch per epoch
+    ts_losses, ts_accs = [], []
+    y_pred = np.array([0])
+    y_true = np.array([0])
+    for _ in xrange(epoch_size):
+        x, y = data.get_next_batch("test", batch_size, num_steps, one_hot=True)
+        x = np.array(x)
+        ts_loss, ts_acc = model.test_on_batch(x, y)
+        ts_losses.append(ts_loss)
+        ts_accs.append(ts_acc)
+        y_pred = np.append(y_pred, np.argmax(model.predict_on_batch(x), axis=1))
+        y_true = np.append(y_true, np.argmax(y, axis=1))
+
+    print("Test Acc: %f\tLoss: %f\tF1 Score: %f" % (
+        sum(ts_accs) / epoch_size, sum(ts_losses) / epoch_size,
+        f1_score(y_true, y_pred, average="macro")))
+    return
+
+
+model_lstm = create_lstm_model(batch_size, num_hidden_units, num_steps, num_features, num_classes)
+train(model_lstm, pamap2_data)
 
 # model_att_hidden = create_attention_time_continuous_model(batch_size, num_hidden_units, num_steps, num_features, num_classes)
 # train(model_att_hidden, pamap2_data)
@@ -63,6 +84,6 @@ def train(model, data):
 # model_att_input = create_attention_input_rnn_continuous_model(batch_size, num_hidden_units, num_steps, num_features, num_classes)
 # train(model_att_input, pamap2_data)
 
-
-model_att_multihead_input = create_attention_input_multihead_model(batch_size, num_hidden_units, num_steps, num_features, num_classes, num_heads)
-train(model_att_multihead_input, pamap2_data)
+#
+# model_att_multihead_input = create_attention_input_multihead_model(batch_size, num_hidden_units, num_steps, num_features, num_classes, num_heads)
+# train(model_att_multihead_input, pamap2_data)
